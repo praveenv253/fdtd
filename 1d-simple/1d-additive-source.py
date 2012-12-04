16bdcc2c388d0e533ab53928a5d94ef3a0b0dd2a
Praveen Venkatesh <praveenv253@gmail.com>
2012-12-04 22:32:42 +0530

    First commit: Added simple 1d implementations.

---
+++ 1d-simple/1d-additive-source.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import scipy as sp
+import matplotlib.pylab as pl
+
+SIZE = 200
+MAXTIME = 500
+
+ez = sp.zeros(SIZE)
+hy = sp.zeros(SIZE)
+imp0 = 377.0
+snapshots = []
+
+for t in range(MAXTIME):
+    hy[:-1] = hy[:-1] + (ez[1:] - ez[:-1]) / imp0
+    
+    ez[1:] = ez[1:] + (hy[1:] - hy[:-1]) * imp0
+    
+    ez[50] += sp.exp(-(t-30) * (t-30) / 100.0)
+    
+    if t % 10 == 0:
+        snapshots.append(ez.copy() + (t / 10)*sp.ones(SIZE))
+
+print 'Done with time stepping'
+
+pl.figure(0)
+for out in snapshots:
+    pl.plot(range(len(out)), out, 'b-')
+pl.show()
---
+++ 1d-simple/1d-tfsf.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import scipy as sp
+import matplotlib.pylab as pl
+
+SIZE = 200
+MAXTIME = 500
+TFSF_POS = 50 # Index of electric field (included) from which total field starts
+INTERFACE = 100 # E-field index (inclusive) from where the new medium starts
+EPSILON_R = 9
+MU_R = 1
+
+ez = sp.zeros(SIZE)
+hy = sp.zeros(SIZE)
+imp0 = 377.0
+snapshots = []
+epsR = sp.ones(SIZE)
+epsR[INTERFACE:] *= EPSILON_R
+muR = sp.ones(SIZE)
+muR[INTERFACE:] *= MU_R
+
+for t in range(MAXTIME):
+    # TODO: Find out why exactly there is a subtle difference in the incremental
+    # electric and magnetic fields. Solution-wise, there is no noticable
+    # difference.
+    ezinc = sp.exp(-(t+0.5-(-0.5)-30) * (t+0.5-(-0.5)-30) / 100.0)
+    hyinc = sp.exp(-(t-30) * (t-30) / 100.0) / (imp0 * muR)
+    
+    # TODO: Find out why the ABCs must be given *before* the corresponding 
+    # update equation. I'd have thought that it should be done *after*.
+    hy[-1] = hy[-2]
+    
+    hy[:-1] = hy[:-1] + (ez[1:] - ez[:-1]) / (imp0 * muR[:-1])
+    
+    hy[TFSF_POS-1] -= hyinc[TFSF_POS-1]
+    
+    ez[0] = ez[1]
+    
+    ez[1:] = ez[1:] + (hy[1:] - hy[:-1]) * imp0 / epsR[1:]
+    
+    ez[TFSF_POS] += ezinc
+    
+    if t % 10 == 0:
+        snapshots.append(ez.copy() + (t / 10)*sp.ones(SIZE))
+
+print 'Done with time stepping'
+
+pl.figure(0)
+for out in snapshots:
+    pl.plot(range(len(out)), out, 'b-')
+pl.show()
