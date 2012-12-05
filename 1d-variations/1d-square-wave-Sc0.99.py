f695a48ff64d108858bc6b6b50ee47d059134145
Praveen Venkatesh <praveenv253@gmail.com>
2012-12-05 21:34:57 +0530

    Add experimental small variations to bares-bones

---
+++ 1d-variations/1d-square-wave-Sc0.99.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import scipy as sp
+import matplotlib.pylab as pl
+
+SIZE = 200
+MAXTIME = 500
+
+ez = sp.zeros(SIZE)
+hy = sp.zeros(SIZE)
+imp0 = 377.0
+Sc = 0.99
+snapshots = []
+
+for t in range(MAXTIME):
+    hy[:-1] = hy[:-1] + (ez[1:] - ez[:-1]) * Sc / imp0
+    
+    ez[1:] = ez[1:] + (hy[1:] - hy[:-1]) * Sc * imp0
+    
+    ez[0] = sp.floor(1.99 * sp.exp(- t*t / 100.0))
+    
+    if t % 10 == 0:
+        snapshots.append(ez.copy() + (t / 5)*sp.ones(SIZE))
+
+print 'Done with time stepping'
+
+pl.figure(0)
+for out in snapshots:
+    pl.plot(range(len(out)), out, 'b-')
+pl.show()
